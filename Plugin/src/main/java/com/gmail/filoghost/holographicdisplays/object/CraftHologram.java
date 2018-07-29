32c202d73a8da35ab2ffa6be59c2203fb7ea73e1
Gabriele C <sgdc3.mail@gmail.com>
2018-07-30 00:47:09 +0200

    isInChunk world check

--- Plugin/src/main/java/com/gmail/filoghost/holographicdisplays/object/CraftHologram.java
+++ Plugin/src/main/java/com/gmail/filoghost/holographicdisplays/object/CraftHologram.java
@@ -49,7 +49,7 @@ public class CraftHologram implements Hologram, com.gmail.filoghost.holograms.ap
 	}
 
 	public boolean isInChunk(Chunk chunk) {
-		return chunk.getX() == chunkX && chunk.getZ() == chunkZ;
+		return chunk.getWorld().equals(world) && chunk.getX() == chunkX && chunk.getZ() == chunkZ;
 	}
 
 	@Override
