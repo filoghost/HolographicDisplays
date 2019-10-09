e4131bbfe465646f99b7bd37386b41d06785f01a
DRC-DEV <dracarys.pro@gmail.com>
2019-10-09 15:39:52 +0200

    Revert the codestyle to be ugly again.

--- Plugin/src/main/java/com/gmail/filoghost/holographicdisplays/bridge/bungeecord/BungeeChannel.java
+++ Plugin/src/main/java/com/gmail/filoghost/holographicdisplays/bridge/bungeecord/BungeeChannel.java
@@ -14,95 +14,97 @@
  */
 package com.gmail.filoghost.holographicdisplays.bridge.bungeecord;
 
-import com.gmail.filoghost.holographicdisplays.HolographicDisplays;
-import com.gmail.filoghost.holographicdisplays.disk.Configuration;
-import com.gmail.filoghost.holographicdisplays.util.ConsoleLogger;
-import com.gmail.filoghost.holographicdisplays.util.NMSVersion;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.logging.Level;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-import java.io.*;
-import java.util.Collection;
-import java.util.logging.Level;
+import com.gmail.filoghost.holographicdisplays.HolographicDisplays;
+import com.gmail.filoghost.holographicdisplays.disk.Configuration;
+import com.gmail.filoghost.holographicdisplays.util.ConsoleLogger;
+import com.gmail.filoghost.holographicdisplays.util.NMSVersion;
 
 public class BungeeChannel implements PluginMessageListener {
 
-    private static BungeeChannel instance;
+	private static BungeeChannel instance;
 
-    public static BungeeChannel getInstance() {
-        if (instance == null) {
-            instance = new BungeeChannel(HolographicDisplays.getInstance());
-        }
-        return instance;
-    }
+	public static BungeeChannel getInstance() {
+		if (instance == null) {
+			instance = new BungeeChannel(HolographicDisplays.getInstance());
+		}
+		return instance;
+	}
 
-    private BungeeChannel(Plugin plugin) {
-        Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
+	private BungeeChannel(Plugin plugin) {
+		Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
         Bukkit.getMessenger().registerIncomingPluginChannel(plugin, "BungeeCord", this);
 
         String targetChannel = "RedisBungee";
-        if (NMSVersion.isGreaterEqualThan(NMSVersion.v1_13_R1)) {
-            targetChannel = "legacy:redisbungee";
-        }
+        if (NMSVersion.isGreaterEqualThan(NMSVersion.v1_13_R1)) targetChannel = "legacy:redisbungee";
+
         Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, targetChannel);
         Bukkit.getMessenger().registerIncomingPluginChannel(plugin, targetChannel, this);
-    }
 
-    @Override
-    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-        String targetChannel = Configuration.useRedisBungee ? "RedisBungee" : "BungeeCord";
-        if (targetChannel.equalsIgnoreCase("RedisBungee") && NMSVersion.isGreaterEqualThan(NMSVersion.v1_13_R1)) {
-            targetChannel = "legacy:redisbungee";
-        }
-        if (channel.equals(targetChannel)) {
-            DataInputStream in = new DataInputStream(new ByteArrayInputStream(message));
+	}
 
-            try {
-                String subChannel = in.readUTF();
+	@Override
+	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+		String targetChannel = Configuration.useRedisBungee ? "RedisBungee" : "BungeeCord";
+        if (targetChannel.equalsIgnoreCase("RedisBungee") && NMSVersion.isGreaterEqualThan(NMSVersion.v1_13_R1)) targetChannel = "legacy:redisbungee";
+		if (channel.equals(targetChannel)) {
+			DataInputStream in = new DataInputStream(new ByteArrayInputStream(message));
 
-                if (subChannel.equals("PlayerCount")) {
-                    String server = in.readUTF();
+			try {
+				String subChannel = in.readUTF();
 
-                    if (in.available() > 0) {
-                        int online = in.readInt();
+				if (subChannel.equals("PlayerCount")) {
+					String server = in.readUTF();
 
-                        BungeeServerInfo serverInfo = BungeeServerTracker.getOrCreateServerInfo(server);
-                        serverInfo.setOnlinePlayers(online);
-                    }
-                }
+					if (in.available() > 0) {
+						int online = in.readInt();
 
-            } catch (EOFException e) {
-                // Do nothing.
-            } catch (IOException e) {
-                // This should never happen.
-                e.printStackTrace();
-            }
-        }
-    }
+						BungeeServerInfo serverInfo = BungeeServerTracker.getOrCreateServerInfo(server);
+						serverInfo.setOnlinePlayers(online);
+					}
+				}
+
+			} catch (EOFException e) {
+				// Do nothing.
+			} catch (IOException e) {
+				// This should never happen.
+				e.printStackTrace();
+			}
+		}
+	}
 
 
-    public void askPlayerCount(String server) {
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
-        DataOutputStream out = new DataOutputStream(b);
+	public void askPlayerCount(String server) {
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		DataOutputStream out = new DataOutputStream(b);
 
-        try {
-            out.writeUTF("PlayerCount");
-            out.writeUTF(server);
-        } catch (IOException e) {
-            // It should not happen.
-            ConsoleLogger.log(Level.WARNING, "I/O Exception while asking for player count on server '" + server + "'.", e);
-        }
+		try {
+			out.writeUTF("PlayerCount");
+			out.writeUTF(server);
+		} catch (IOException e) {
+			// It should not happen.
+			ConsoleLogger.log(Level.WARNING, "I/O Exception while asking for player count on server '" + server + "'.", e);
+		}
 
-        // OR, if you don't need to send it to a specific player
-        Collection<? extends Player> players = Bukkit.getOnlinePlayers();
-        if (players.size() > 0) {
+		// OR, if you don't need to send it to a specific player
+		Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+		if (players.size() > 0) {
             String targetChannel = Configuration.useRedisBungee ? "RedisBungee" : "BungeeCord";
-            if (targetChannel.equalsIgnoreCase("RedisBungee") && NMSVersion.isGreaterEqualThan(NMSVersion.v1_13_R1)) {
-                targetChannel = "legacy:redisbungee";
-            }
-            players.iterator().next().sendPluginMessage(HolographicDisplays.getInstance(), targetChannel, b.toByteArray());
-        }
-    }
+            if (targetChannel.equalsIgnoreCase("RedisBungee") && NMSVersion.isGreaterEqualThan(NMSVersion.v1_13_R1)) targetChannel = "legacy:redisbungee";
+			players.iterator().next().sendPluginMessage(HolographicDisplays.getInstance(), targetChannel, b.toByteArray());
+		}
+	}
 }
