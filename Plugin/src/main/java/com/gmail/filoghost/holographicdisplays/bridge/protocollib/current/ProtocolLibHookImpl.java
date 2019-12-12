e5bf5ce81844f1e347d213c5ff28ea43e8b0c86a
Chazza <me@chazza.net>
2019-12-12 21:57:26 +0000

    Added PlaceholderAPI support.

--- Plugin/src/main/java/com/gmail/filoghost/holographicdisplays/bridge/protocollib/current/ProtocolLibHookImpl.java
+++ Plugin/src/main/java/com/gmail/filoghost/holographicdisplays/bridge/protocollib/current/ProtocolLibHookImpl.java
@@ -3,24 +3,16 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package com.gmail.filoghost.holographicdisplays.bridge.protocollib.current;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 import com.comphenix.net.sf.cglib.proxy.Factory;
 import com.comphenix.protocol.PacketType;
@@ -37,28 +29,33 @@ import com.gmail.filoghost.holographicdisplays.bridge.protocollib.current.packet
 import com.gmail.filoghost.holographicdisplays.bridge.protocollib.current.packet.WrapperPlayServerEntityMetadata;
 import com.gmail.filoghost.holographicdisplays.bridge.protocollib.current.packet.WrapperPlayServerSpawnEntity;
 import com.gmail.filoghost.holographicdisplays.bridge.protocollib.current.packet.WrapperPlayServerSpawnEntityLiving;
+import com.gmail.filoghost.holographicdisplays.hook.PlaceholderAPIHook;
 import com.gmail.filoghost.holographicdisplays.nms.interfaces.NMSManager;
 import com.gmail.filoghost.holographicdisplays.nms.interfaces.entity.NMSArmorStand;
 import com.gmail.filoghost.holographicdisplays.nms.interfaces.entity.NMSEntityBase;
 import com.gmail.filoghost.holographicdisplays.object.CraftHologram;
-import com.gmail.filoghost.holographicdisplays.object.line.CraftHologramLine;
-import com.gmail.filoghost.holographicdisplays.object.line.CraftItemLine;
-import com.gmail.filoghost.holographicdisplays.object.line.CraftTextLine;
-import com.gmail.filoghost.holographicdisplays.object.line.CraftTouchSlimeLine;
-import com.gmail.filoghost.holographicdisplays.object.line.CraftTouchableLine;
+import com.gmail.filoghost.holographicdisplays.object.line.*;
 import com.gmail.filoghost.holographicdisplays.placeholder.RelativePlaceholder;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * This is for the ProtocolLib versions containing the WrappedDataWatcher.WrappedDataWatcherObject class.
  */
 public class ProtocolLibHookImpl implements ProtocolLibHook {
-	
+
 	private NMSManager nmsManager;
 	private PacketHelper packetHelper;
 	private MetadataHelper metadataHelper;
 	private boolean useGetEntityWorkaround;
-	
-	
+
+
 	@Override
 	public boolean hook(Plugin plugin, NMSManager nmsManager) {
 		this.nmsManager = nmsManager;
@@ -66,110 +63,114 @@ public class ProtocolLibHookImpl implements ProtocolLibHook {
 		this.packetHelper = new PacketHelper(metadataHelper);
 
 		AdapterParameteters params = PacketAdapter
-			.params()
-			.plugin(plugin)
-			.types(
-				PacketType.Play.Server.SPAWN_ENTITY_LIVING,
-				PacketType.Play.Server.SPAWN_ENTITY,
-				PacketType.Play.Server.ENTITY_METADATA)
-			.serverSide()
-			.listenerPriority(ListenerPriority.NORMAL);
-		
+				.params()
+				.plugin(plugin)
+				.types(
+						PacketType.Play.Server.SPAWN_ENTITY_LIVING,
+						PacketType.Play.Server.SPAWN_ENTITY,
+						PacketType.Play.Server.ENTITY_METADATA)
+				.serverSide()
+				.listenerPriority(ListenerPriority.NORMAL);
+
 		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(params) {
-					  
-				@Override
-				public void onPacketSending(PacketEvent event) {
-					
-					PacketContainer packet = event.getPacket();
-					Player player = event.getPlayer();
-					
-					if (player instanceof Factory) {
-						return; // Ignore temporary players (reference: https://github.com/dmulloy2/ProtocolLib/issues/349)
+
+			@Override
+			public void onPacketSending(PacketEvent event) {
+
+				PacketContainer packet = event.getPacket();
+				Player player = event.getPlayer();
+
+				if (player instanceof Factory) {
+					return; // Ignore temporary players (reference: https://github.com/dmulloy2/ProtocolLib/issues/349)
+				}
+
+				// Spawn entity packet
+				if (packet.getType() == PacketType.Play.Server.SPAWN_ENTITY_LIVING) {
+
+					WrapperPlayServerSpawnEntityLiving spawnEntityPacket = new WrapperPlayServerSpawnEntityLiving(packet);
+					Entity entity = getEntity(event, spawnEntityPacket);
+
+					CraftHologramLine hologramLine = getHologramLine(entity);
+					if (hologramLine == null) {
+						return;
 					}
 
-					// Spawn entity packet
-					if (packet.getType() == PacketType.Play.Server.SPAWN_ENTITY_LIVING) {
+					if (!hologramLine.getParent().getVisibilityManager().isVisibleTo(player)) {
+						event.setCancelled(true);
+						return;
+					}
 
-						WrapperPlayServerSpawnEntityLiving spawnEntityPacket = new WrapperPlayServerSpawnEntityLiving(packet);
-						Entity entity = getEntity(event, spawnEntityPacket);
-						
-						CraftHologramLine hologramLine = getHologramLine(entity);
-						if (hologramLine == null) {
-							return;
-						}
-						
-						if (!hologramLine.getParent().getVisibilityManager().isVisibleTo(player)) {
-							event.setCancelled(true);
-							return;
-						}
-						
-						if (!hologramLine.getParent().isAllowPlaceholders() || !hologramLine.hasRelativePlaceholders()) {
-							return;
-						}
-						
-						spawnEntityPacket = new WrapperPlayServerSpawnEntityLiving(packet.deepClone());
-						WrappedWatchableObject customNameWatchableObject = metadataHelper.getCustomNameWacthableObject(spawnEntityPacket.getMetadata());
-						
-						if (customNameWatchableObject == null) {
-							return;
-						}
-						
-						replaceRelativePlaceholders(customNameWatchableObject, player, hologramLine.getRelativePlaceholders());
+					if (!hologramLine.getParent().isAllowPlaceholders()) {
+						return;
+					}
+
+					spawnEntityPacket = new WrapperPlayServerSpawnEntityLiving(packet.deepClone());
+					WrappedWatchableObject customNameWatchableObject = metadataHelper.getCustomNameWacthableObject(spawnEntityPacket.getMetadata());
+
+					if (customNameWatchableObject == null) {
+						return;
+					}
+
+					boolean modifiedRelative = replaceRelativePlaceholders(customNameWatchableObject, player, hologramLine.getRelativePlaceholders());
+					boolean modifiedPapi = replacePlaceholderAPI(customNameWatchableObject, player);
+					if (modifiedRelative || modifiedPapi) {
 						event.setPacket(spawnEntityPacket.getHandle());
+					}
 
-					} else if (packet.getType() == PacketType.Play.Server.SPAWN_ENTITY) {
+				} else if (packet.getType() == PacketType.Play.Server.SPAWN_ENTITY) {
 
-						WrapperPlayServerSpawnEntity spawnEntityPacket = new WrapperPlayServerSpawnEntity(packet);
-						Entity entity = getEntity(event, spawnEntityPacket);
-						
-						CraftHologramLine hologramLine = getHologramLine(entity);
-						if (hologramLine == null) {
-							return;
-						}
-						
-						if (!hologramLine.getParent().getVisibilityManager().isVisibleTo(player)) {
-							event.setCancelled(true);
-							return;
-						}
-					
-					} else if (packet.getType() == PacketType.Play.Server.ENTITY_METADATA) {
+					WrapperPlayServerSpawnEntity spawnEntityPacket = new WrapperPlayServerSpawnEntity(packet);
+					Entity entity = getEntity(event, spawnEntityPacket);
 
-						WrapperPlayServerEntityMetadata entityMetadataPacket = new WrapperPlayServerEntityMetadata(packet);
-						Entity entity = getEntity(event, entityMetadataPacket);
-						
-						CraftHologramLine hologramLine = getHologramLine(entity);
-						if (hologramLine == null) {
-							return;
-						}
-						
-						if (!hologramLine.getParent().getVisibilityManager().isVisibleTo(player)) {
-							event.setCancelled(true);
-							return;
-						}
-						
-						if (!hologramLine.getParent().isAllowPlaceholders() || !hologramLine.hasRelativePlaceholders()) {
-							return;
-						}
-						
-						entityMetadataPacket = new WrapperPlayServerEntityMetadata(packet.deepClone());
-						WrappedWatchableObject customNameWatchableObject = metadataHelper.getCustomNameWatchableObject(entityMetadataPacket.getEntityMetadata());
-						
-						if (customNameWatchableObject == null) {
-							return;
-						}
-						
-						boolean modified = replaceRelativePlaceholders(customNameWatchableObject, player, hologramLine.getRelativePlaceholders());
-						if (modified) {
-							event.setPacket(entityMetadataPacket.getHandle());
-						}
+					CraftHologramLine hologramLine = getHologramLine(entity);
+					if (hologramLine == null) {
+						return;
+					}
+
+					if (!hologramLine.getParent().getVisibilityManager().isVisibleTo(player)) {
+						event.setCancelled(true);
+						return;
+					}
+
+				} else if (packet.getType() == PacketType.Play.Server.ENTITY_METADATA) {
+
+					WrapperPlayServerEntityMetadata entityMetadataPacket = new WrapperPlayServerEntityMetadata(packet);
+					Entity entity = getEntity(event, entityMetadataPacket);
+
+					CraftHologramLine hologramLine = getHologramLine(entity);
+					if (hologramLine == null) {
+						return;
+					}
+
+					if (!hologramLine.getParent().getVisibilityManager().isVisibleTo(player)) {
+						event.setCancelled(true);
+						return;
+					}
+
+					if (!hologramLine.getParent().isAllowPlaceholders()) {
+						return;
+					}
+
+					entityMetadataPacket = new WrapperPlayServerEntityMetadata(packet.deepClone());
+					WrappedWatchableObject customNameWatchableObject = metadataHelper.getCustomNameWatchableObject(entityMetadataPacket.getEntityMetadata());
+
+					if (customNameWatchableObject == null) {
+						return;
+					}
+
+					boolean modifiedRelative = replaceRelativePlaceholders(customNameWatchableObject, player, hologramLine.getRelativePlaceholders());
+					boolean modifiedPapi = replacePlaceholderAPI(customNameWatchableObject, player);
+					if (modifiedRelative || modifiedPapi) {
+						event.setPacket(entityMetadataPacket.getHandle());
 					}
 				}
-			});
-		
+			}
+		});
+
 		return true;
 	}
-	
-	
+
+
 	private Entity getEntity(PacketEvent packetEvent, EntityRelatedPacketWrapper packetWrapper) {
 		if (!useGetEntityWorkaround) {
 			try {
@@ -178,31 +179,49 @@ public class ProtocolLibHookImpl implements ProtocolLibHook {
 				useGetEntityWorkaround = true;
 			}
 		}
-		
+
 		// Use workaround, get entity from its ID through NMS
 		return HolographicDisplays.getNMSManager().getEntityFromID(packetEvent.getPlayer().getWorld(), packetWrapper.getEntityID());
 	}
-	
-	
+
+
 	private boolean replaceRelativePlaceholders(WrappedWatchableObject customNameWatchableObject, Player player, Collection<RelativePlaceholder> relativePlaceholders) {
 		if (customNameWatchableObject == null) {
 			return true;
 		}
-		
+
 		String customName = metadataHelper.getSerializedCustomName(customNameWatchableObject);
 		if (customName == null) {
 			return false;
 		}
-		
+
+		if (relativePlaceholders == null) return false;
+
 		for (RelativePlaceholder relativePlaceholder : relativePlaceholders) {
 			customName = customName.replace(relativePlaceholder.getTextPlaceholder(), relativePlaceholder.getReplacement(player));
 		}
-		
+
 		metadataHelper.setSerializedCustomName(customNameWatchableObject, customName);
 		return true;
 	}
-	
-	
+
+	private boolean replacePlaceholderAPI(WrappedWatchableObject customNameWatchableObject, Player player) {
+		if (customNameWatchableObject == null) {
+			return true;
+		}
+
+		String customName = metadataHelper.getSerializedCustomName(customNameWatchableObject);
+		if (customName == null) {
+			return false;
+		}
+
+		customName = PlaceholderAPIHook.translate(player, customName);
+
+		metadataHelper.setSerializedCustomName(customNameWatchableObject, customName);
+		return true;
+	}
+
+
 	@Override
 	public void sendDestroyEntitiesPacket(Player player, CraftHologram hologram) {
 		List<Integer> ids = new ArrayList<>();
@@ -213,58 +232,58 @@ public class ProtocolLibHookImpl implements ProtocolLibHook {
 				}
 			}
 		}
-		
+
 		if (!ids.isEmpty()) {
 			packetHelper.sendDestroyEntitiesPacket(player, ids);
 		}
 	}
-	
-	
+
+
 	@Override
 	public void sendDestroyEntitiesPacket(Player player, CraftHologramLine line) {
 		if (!line.isSpawned()) {
 			return;
 		}
-		
+
 		List<Integer> ids = new ArrayList<>();
 		for (int id : line.getEntitiesIDs()) {
 			ids.add(id);
 		}
-		
+
 		if (!ids.isEmpty()) {
 			packetHelper.sendDestroyEntitiesPacket(player, ids);
 		}
 	}
-	
-	
+
+
 	@Override
 	public void sendCreateEntitiesPacket(Player player, CraftHologram hologram) {
 		for (CraftHologramLine line : hologram.getLinesUnsafe()) {
 			sendCreateEntitiesPacket(player, line);
 		}
 	}
-	
-	
+
+
 	@Override
 	public void sendCreateEntitiesPacket(Player player, CraftHologramLine line) {
 		if (!line.isSpawned()) {
 			return;
 		}
-		
+
 		CraftTouchableLine touchableLine;
-		
+
 		if (line instanceof CraftTextLine) {
 			CraftTextLine textLine = (CraftTextLine) line;
 			touchableLine = textLine;
-			
+
 			if (textLine.isSpawned()) {
 				packetHelper.sendSpawnArmorStandPacket(player, (NMSArmorStand) textLine.getNmsNameable());
 			}
-			
+
 		} else if (line instanceof CraftItemLine) {
 			CraftItemLine itemLine = (CraftItemLine) line;
 			touchableLine = itemLine;
-			
+
 			if (itemLine.isSpawned()) {
 				packetHelper.sendSpawnArmorStandPacket(player, (NMSArmorStand) itemLine.getNmsVehicle());
 				packetHelper.sendSpawnItemPacket(player, itemLine.getNmsItem());
@@ -274,31 +293,31 @@ public class ProtocolLibHookImpl implements ProtocolLibHook {
 		} else {
 			throw new IllegalArgumentException("Unexpected hologram line type: " + line.getClass().getName());
 		}
-		
+
 		if (touchableLine != null && touchableLine.isSpawned() && touchableLine.getTouchSlime() != null) {
 			CraftTouchSlimeLine touchSlime = touchableLine.getTouchSlime();
-			
+
 			if (touchSlime.isSpawned()) {
 				packetHelper.sendSpawnArmorStandPacket(player, (NMSArmorStand) touchSlime.getNmsVehicle());
-				packetHelper.sendSpawnSlimePacket(player, touchSlime.getNmsSlime());				
+				packetHelper.sendSpawnSlimePacket(player, touchSlime.getNmsSlime());
 				packetHelper.sendVehicleAttachPacket(player, touchSlime.getNmsVehicle(), touchSlime.getNmsSlime());
 			}
 		}
 	}
-	
-	
+
+
 	private CraftHologramLine getHologramLine(Entity bukkitEntity) {
-		if (bukkitEntity != null && isHologramType(bukkitEntity.getType())) {		
+		if (bukkitEntity != null && isHologramType(bukkitEntity.getType())) {
 			NMSEntityBase entity = nmsManager.getNMSEntityBase(bukkitEntity);
 			if (entity != null) {
 				return (CraftHologramLine) entity.getHologramLine();
 			}
 		}
-		
+
 		return null;
 	}
-	
-	
+
+
 	private boolean isHologramType(EntityType type) {
 		return type == EntityType.ARMOR_STAND || type == EntityType.DROPPED_ITEM || type == EntityType.SLIME;
 	}
---
+++ Plugin/src/main/java/com/gmail/filoghost/holographicdisplays/hook/PlaceholderAPIHook.java
@@ -0,0 +1,18 @@
+package com.gmail.filoghost.holographicdisplays.hook;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class PlaceholderAPIHook {
+    private static boolean enabled;
+
+    public PlaceholderAPIHook() {
+        enabled = Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI");
+    }
+
+    public static String translate(Player player, String str) {
+        if (player == null || !enabled) return str;
+        return PlaceholderAPI.setPlaceholders(player, str);
+    }
+}
