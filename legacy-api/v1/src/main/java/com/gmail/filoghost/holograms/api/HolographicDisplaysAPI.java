1906cadf961a9b4c74cf5786547306ba7563b71f
filoghost <filoghost@gmail.com>
2021-05-10 22:48:24 +0200

    Remove the v1 API and start throwing exceptions

--- legacy-api/v1/src/main/java/com/gmail/filoghost/holograms/api/HolographicDisplaysAPI.java
+++ legacy-api/v1/src/main/java/com/gmail/filoghost/holograms/api/HolographicDisplaysAPI.java
@@ -5,64 +5,79 @@
  */
 package com.gmail.filoghost.holograms.api;
 
-import com.gmail.filoghost.holograms.api.internal.NewAPIAdapter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @deprecated Please use the new API!
  */
 @Deprecated
 public class HolographicDisplaysAPI {
+
+    private static final Set<String> notifiedPlugins = new HashSet<>();
     
     @Deprecated
     public static Hologram createHologram(Plugin plugin, Location source, String... lines) {
-        return NewAPIAdapter.createHologram(plugin, source, lines);
+        throw removedAPIException(plugin);
     }
 
     @Deprecated
     public static FloatingItem createFloatingItem(Plugin plugin, Location source, ItemStack itemstack) {
-        return NewAPIAdapter.createFloatingItem(plugin, source, itemstack);
+        throw removedAPIException(plugin);
     }
 
     @Deprecated
     public static Hologram createIndividualHologram(Plugin plugin, Location source, Player whoCanSee, String... lines) {
-        return NewAPIAdapter.createIndividualHologram(plugin, source, whoCanSee, lines);
+        throw removedAPIException(plugin);
     }
     
     @Deprecated
     public static Hologram createIndividualHologram(Plugin plugin, Location source, List<Player> whoCanSee, String... lines) {
-        return NewAPIAdapter.createIndividualHologram(plugin, source, whoCanSee, lines);
+        throw removedAPIException(plugin);
     }
     
     @Deprecated
     public static FloatingItem createIndividualFloatingItem(Plugin plugin, Location source, Player whoCanSee, ItemStack itemstack) {
-        return NewAPIAdapter.createIndividualFloatingItem(plugin, source, whoCanSee, itemstack);
+        throw removedAPIException(plugin);
     }
     
     @Deprecated
     public static FloatingItem createIndividualFloatingItem(Plugin plugin, Location source, List<Player> whoCanSee, ItemStack itemstack) {
-        return NewAPIAdapter.createIndividualFloatingItem(plugin, source, whoCanSee, itemstack);
+        throw removedAPIException(plugin);
     }
     
     @Deprecated
     public static Hologram[] getHolograms(Plugin plugin) {
-        return NewAPIAdapter.getHolograms(plugin);
+        throw removedAPIException(plugin);
     }
-    
+
     @Deprecated
     public static FloatingItem[] getFloatingItems(Plugin plugin) {
-        return NewAPIAdapter.getFloatingItems(plugin);
+        throw removedAPIException(plugin);
     }
     
+    private static RuntimeException removedAPIException(Plugin plugin) {
+        if (plugin != null && notifiedPlugins.add(plugin.getName())) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[Holographic Displays] The plugin \""
+                    + plugin.getName() + "\" is still using the API v1 of Holographic Displays,"
+                    + "which has been removed. Please notify its author.");
+        }
+
+        return new IllegalStateException("The legacy Holographic Displays API v1 has been removed");
+    }
+
     @Deprecated
     public static boolean isHologramEntity(Entity bukkitEntity) {
-        return NewAPIAdapter.isHologramEntity(bukkitEntity);
+        return me.filoghost.holographicdisplays.api.HolographicDisplaysAPI.isHologramEntity(bukkitEntity);
     }
 
 }
