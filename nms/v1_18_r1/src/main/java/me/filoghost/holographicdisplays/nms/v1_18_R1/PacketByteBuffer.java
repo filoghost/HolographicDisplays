9be9ecb2acd9901db11cf3e81f34d4f7fde5abad
filoghost <filoghost@gmail.com>
2021-12-01 19:20:06 +0100

    Add support for Minecraft 1.18

---
+++ nms/v1_18_r1/src/main/java/me/filoghost/holographicdisplays/nms/v1_18_R1/PacketByteBuffer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) filoghost and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+package me.filoghost.holographicdisplays.nms.v1_18_R1;
+
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.PacketDataSerializer;
+
+import java.util.UUID;
+
+class PacketByteBuffer extends PacketDataSerializer {
+
+    private static final PacketByteBuffer INSTANCE = new PacketByteBuffer();
+
+    static PacketByteBuffer get() {
+        INSTANCE.clear();
+        return INSTANCE;
+    }
+
+    private PacketByteBuffer() {
+        super(Unpooled.buffer());
+    }
+
+    void writeVarInt(int i) {
+        super.d(i);
+    }
+
+    void writeVarIntArray(int i1) {
+        writeVarInt(1);
+        writeVarInt(i1);
+    }
+
+    void writeVarIntArray(int i1, int i2) {
+        writeVarInt(2);
+        writeVarInt(i1);
+        writeVarInt(i2);
+    }
+
+    void writeUUID(UUID uuid) {
+        super.a(uuid);
+    }
+
+    <T> void writeDataWatcherEntry(DataWatcherKey<T> key, T value) {
+        writeByte(key.getIndex());
+        writeVarInt(key.getSerializerTypeID());
+        key.getSerializer().a(this, value);
+    }
+
+    void writeDataWatcherEntriesEnd() {
+        writeByte(0xFF);
+    }
+
+}
