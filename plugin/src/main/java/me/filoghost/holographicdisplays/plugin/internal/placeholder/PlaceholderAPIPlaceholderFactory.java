b78de9eeea0e7ee2138caadf9219e6b9c8cd37cc
filoghost <filoghost@gmail.com>
2021-12-09 19:27:35 +0100

    Re-implement PlaceholderAPI as normal placeholder

---
+++ plugin/src/main/java/me/filoghost/holographicdisplays/plugin/internal/placeholder/PlaceholderAPIPlaceholderFactory.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) filoghost and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+package me.filoghost.holographicdisplays.plugin.internal.placeholder;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.filoghost.holographicdisplays.api.beta.placeholder.IndividualPlaceholder;
+import me.filoghost.holographicdisplays.api.beta.placeholder.IndividualPlaceholderFactory;
+import me.filoghost.holographicdisplays.plugin.bridge.placeholderapi.PlaceholderAPIHook;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PlaceholderAPIPlaceholderFactory implements IndividualPlaceholderFactory {
+
+    @Override
+    public @Nullable IndividualPlaceholder getPlaceholder(@Nullable String argument) {
+        if (argument == null) {
+            return null;
+        }
+        return new PlaceholderAPIPlaceholder("%" + argument + "%");
+    }
+
+
+    private static class PlaceholderAPIPlaceholder implements IndividualPlaceholder {
+
+        private final String content;
+
+        PlaceholderAPIPlaceholder(String content) {
+            this.content = content;
+        }
+
+        @Override
+        public int getRefreshIntervalTicks() {
+            return 1;
+        }
+
+        @Override
+        public @Nullable String getReplacement(@NotNull Player player, @Nullable String argument) {
+            if (!PlaceholderAPIHook.isEnabled()) {
+                return null;
+            }
+            return PlaceholderAPI.setPlaceholders(player, content);
+        }
+
+    }
+
+}
