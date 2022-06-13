6220bd2860c36fb7e7c2f3e579e041df5a203b68
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2022-06-12 17:08:20 -0700

    1.19_R1 support

---
+++ nms/v1_19_r1/src/main/java/me/filoghost/holographicdisplays/nms/v1_19_R1/EntityTypeID.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) filoghost and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+package me.filoghost.holographicdisplays.nms.v1_19_R1;
+
+import net.minecraft.core.IRegistry;
+import net.minecraft.world.entity.EntityTypes;
+
+class EntityTypeID {
+
+    static final int ARMOR_STAND = IRegistry.X.a(EntityTypes.d);
+    static final int ITEM = IRegistry.X.a(EntityTypes.T);
+    static final int SLIME = IRegistry.X.a(EntityTypes.aG);
+
+}
