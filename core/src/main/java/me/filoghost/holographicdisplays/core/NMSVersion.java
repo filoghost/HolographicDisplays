48f984503ddc1a910da16387f502efd025083c00
rayanbzd <bzdi.rayan@gmail.com>
2023-12-14 19:42:02 +0100

    1.20.3 & 1.20.4 support

--- core/src/main/java/me/filoghost/holographicdisplays/core/NMSVersion.java
+++ core/src/main/java/me/filoghost/holographicdisplays/core/NMSVersion.java
@@ -42,7 +42,8 @@ public enum NMSVersion {
     /* 1.19.3          */ v1_19_R2(errorCollector -> new me.filoghost.holographicdisplays.nms.v1_19_R2.VersionNMSManager(errorCollector)),
     /* 1.19.4          */ v1_19_R3(errorCollector -> new me.filoghost.holographicdisplays.nms.v1_19_R3.VersionNMSManager(errorCollector)),
     /* 1.20 - 1.20.1   */ v1_20_R1(errorCollector -> new me.filoghost.holographicdisplays.nms.v1_20_R1.VersionNMSManager(errorCollector)),
-    /* 1.20.2 - ?      */ v1_20_R2(errorCollector -> new me.filoghost.holographicdisplays.nms.v1_20_R2.VersionNMSManager(errorCollector)),
+    /* 1.20.2          */ v1_20_R2(errorCollector -> new me.filoghost.holographicdisplays.nms.v1_20_R2.VersionNMSManager(errorCollector)),
+    /* 1.20.3 - 1.20.4 */ v1_20_R3(errorCollector -> new me.filoghost.holographicdisplays.nms.v1_20_R3.VersionNMSManager(errorCollector)),
     /* Other versions  */ UNKNOWN(NMSManagerFactory.unknownVersion());
 
     private static final NMSVersion CURRENT_VERSION = detectCurrentVersion();
